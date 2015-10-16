758cb6c0f4f73b95861d25b1f7d6aadff31fbcf5
givewebsite <houseofcommons69@gmail.com>
2015-10-16 05:00:56 -0500

    commit

--- java.js
+++ java.js
@@ -1,3 +1,10 @@
 
-var myHeading = document.querySelector('h1');
-myHeading.innerHTML = 'Hello world!';
+
+
+
+//paste this code under head tag or in a seperate js file.
+	// Wait for window load
+	$(window).load(function() {
+		// Animate loader off screen
+		$(".se-pre-con").fadeOut("slow");;
+	});
